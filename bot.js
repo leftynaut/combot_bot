33691c29bfde94ee18aa6a01d8ef48eb75a20639
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-11-17 20:46:31 -0600

    es6 update

--- bot.js
+++ bot.js
@@ -1,34 +1,34 @@
-var twitter = require('twitter')
-var config = require('./auth.js')
-var events = require('events')
+const twitter = require('twitter');
+const config = require('./auth.js');
+const events = require('events');
 
-var connect = new twitter(config)
-var eventEmitter = new events.EventEmitter()
-var keywords = {track: '@combatgent'}
-var stream = connect.stream('statuses/filter', keywords)
+const connect = new twitter(config);
+const eventEmitter = new events.EventEmitter();
+const keywords = { track: '@combatgent' };
+const stream = connect.stream('statuses/filter', keywords);
 
-var reply = function (data) {
-  var tweetId = data.data.id_str
-  var screenName = data.data.user.screen_name
-  if (screenName === 'vishaalmelwani') { return }
-  var thePost = '@' + screenName + ' FYI: Combat Gent may be out of business. See here: http://bit.ly/combotgent'
+function reply(data) {
+  const tweetId = data.data.id_str;
+  const screenName = data.data.user.screen_name;
+  if (screenName === 'vishaalmelwani') { return; }
+  const thePost = `@${screenName} FYI: Combat Gent may be out of business. See here: http://bit.ly/combotgent`;
   connect.post('statuses/update', {
     status: thePost,
-    in_reply_to_status_id: tweetId
-  }, function (err, tweet, response) {
+    in_reply_to_status_id: tweetId,
+  }, (err, tweet) => {
     if (err) {
-      console.log('error', err)
+      console.log('error', err);
     } else {
-      console.log('tweeted', tweet)
+      console.log('tweeted', tweet);
     }
-  })
+  });
 }
 
-eventEmitter.on('replyTo', reply)
+eventEmitter.on('replyTo', reply);
 
-stream.on('data', function (data) {
-  eventEmitter.emit('replyTo', {data: data})
-})
-stream.on('error', function (err) {
-  console.log(err)
-})
+stream.on('data', (data) => {
+  eventEmitter.emit('replyTo', { data });
+});
+stream.on('error', (err) => {
+  console.log(err);
+});
